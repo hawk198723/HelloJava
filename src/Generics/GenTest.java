b0fc51467452c4e8e22f1cc14c787b7476d4d73b
hawk198723 <hawk198723@gmail.com>
2022-10-14 18:42:56 -0700

    Generic Exe test

---
+++ src/Generics/GenTest.java
@@ -0,0 +1,30 @@
+package Generics;
+
+public class GenTest {
+    //  printArray
+    public static < E > void printArray( E[] inputArray )
+    {
+        // for loop printout
+        for ( E element : inputArray ){
+            System.out.printf( "%s ", element );
+        }
+        System.out.println();
+    }
+    
+    public static void main( String[] args )
+    {
+        //create different arrays: Integer, Double 和 Character
+        Integer[] intArray = { 1, 2, 3, 4, 5 };
+        Double[] doubleArray = { 1.1, 2.2, 3.3, 4.4 };
+        Character[] charArray = { 'H', 'E', 'L', 'L', 'O' };
+
+        System.out.println( "Integer Array elements are:" );
+        printArray( intArray ); // pass in Integer Array
+
+        System.out.println( "\nDouble Array elements are:" );
+        printArray( doubleArray ); // pass in Double Array
+
+        System.out.println( "\nChar Array elements are:" );
+        printArray( charArray ); // pass in Char Array
+    }
+}
